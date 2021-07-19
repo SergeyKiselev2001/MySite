f26933c0795548c65c958a682e440d61c97c0ff0
Sergey Kiselev <kissergey98@mail.ru>
2021-07-19 22:32:56 +0300

    fifth commit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const PORT = 5000;
 var server = express();
 
+//Main page
 server.get('/', (req, res)=>{
-
-  res.end('Haha!');
+  res.sendFile(path.resolve(__dirname, 'frontend','index.html'));
 });
 
 
