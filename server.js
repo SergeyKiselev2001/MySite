ed6da68088129ab0cc1fd8c8155581eb7fbc1ee6
Sergey Kiselev <kissergey98@mail.ru>
2021-07-19 21:19:59 +0300

    fourth commit

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ import express from "express";
 const PORT = 5000;
 var server = express();
 
+server.get('/', (req, res)=>{
+
+  res.end('Haha!');
+});
+
+
 server.listen(PORT, ()=>{
   console.log(`server has been started on port ${PORT}...`);
 });
