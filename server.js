bc17642f08cb13a70a4fcd717faef170b5e2043b
Sergey Kiselev <kissergey98@mail.ru>
2021-07-19 20:33:47 +0300

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
+    console.log('smth');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World!');
     res.end();
