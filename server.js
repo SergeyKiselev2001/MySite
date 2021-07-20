a0bcce450df1a9c4be7d90132c08359c3fea7303
Sergey Kiselev <kissergey98@mail.ru>
2021-07-20 13:31:07 +0300

    ejs partials

--- server.js
+++ server.js
@@ -11,20 +11,23 @@ var server = express();
 server.set('view engine', 'ejs');
 
 // переназначили папку views --> templates
-server.set('views', path.resolve(__dirname, 'templates'));
+server.set('views', path.resolve(__dirname, 'ejs'));
 console.log(server.get('views'));
 
-
-server.use(express.static(path.resolve(__dirname, "frontend")));
- //подключение middleware
+// уже не надо так как перешли на ejs
+//server.use(express.static(path.resolve(__dirname, "frontend")));
+//подключение middleware
 server.use(dateTime, logger);
 // instead
 //server.get('/features', (req, res)=>{res.sendFile(__dirname, 'frontend', 'features.html')});
 
 
 server.get('/', (req, res)=>{
- 
-  res.sendFile(path.resolve(__dirname, 'frontend','index.html'));
+  res.render('indexx', {name: {haha : 22}});
+});
+
+server.get('/features', (req, res)=>{
+  res.render('indexx', {name : {haha: 'features'}});
 });
 
 //Download index
